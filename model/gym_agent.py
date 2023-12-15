9b278df8cb03d04b909723788d33c2afca7114ca
Andrej Jočić <andrej.jocic.aj@gmail.com>
2023-12-15 12:01:21 +0100

    visualisation workin

--- model/gym_agent.py
+++ model/gym_agent.py
@@ -7,30 +7,27 @@ from collections import Counter
 from typing import List, Iterator, Optional, Dict, Any
 
 
+class State(Enum):
+    SEARCHING = auto()
+    """looking for a machine to use"""
+    # WARMING_UP = auto()
+    WORKING_OUT = auto()
+    """doing a set"""
+    RESTING = auto()
+    """resting between sets"""
+
+Routine = Enum("Routine", "PUSH PULL LEGS TEST") # TODO: remove TEST
+
+Muscle = Enum("Muscle",
+    names= """
+    BICEPS TRICEPS
+    FRONT_DELTS SIDE_DELTS REAR_DELTS
+    CHEST
+    TRAPS LATS
+    QUADS HAMSTRINGS GLUTES CALVES"""
+)
 
 class GymRat(mesa.Agent):
-
-    class State(Enum):
-        SEARCHING = auto()
-        """looking for a machine to use"""
-        # WARMING_UP = auto()
-        WORKING_OUT = auto()
-        """doing a set"""
-        RESTING = auto()
-        """resting between sets"""
-
-    Routine = Enum("Routine", "PUSH PULL LEGS TEST") # TODO: remove TEST
-
-    Muscle = Enum("Muscle",
-        names= """
-        BICEPS TRICEPS
-        FRONT_DELTS SIDE_DELTS REAR_DELTS
-        CHEST
-        TRAPS LATS
-        QUADS HAMSTRINGS GLUTES CALVES"""
-    )
-
-
     unique_id: int
     model: Gym
     state: State
@@ -38,36 +35,35 @@ class GymRat(mesa.Agent):
     """muscle -> number of exercises left to do for that muscle"""
     # TODO: add history of used machines (to make sure the same machine isn't used twice in a workout)
 
-
     def __init__(self, unique_id: int, model: Gym, routine: Routine = Routine.TEST):
         super().__init__(unique_id, model)
-        self.state = GymRat.State.SEARCHING
+        self.state = State.SEARCHING
 
         match routine:
-            case GymRat.Routine.PUSH:
+            case Routine.PUSH:
                 self.training_queue = Counter({
-                    GymRat.Muscle.CHEST: 2,
-                    GymRat.Muscle.TRICEPS: 1,
-                    GymRat.Muscle.FRONT_DELTS: 1,
-                    GymRat.Muscle.SIDE_DELTS: 1,
+                    Muscle.CHEST: 2,
+                    Muscle.TRICEPS: 1,
+                    Muscle.FRONT_DELTS: 1,
+                    Muscle.SIDE_DELTS: 1,
                 })
-            case GymRat.Routine.PULL:
+            case Routine.PULL:
                 self.training_queue = Counter({
-                    GymRat.Muscle.LATS: 2,
-                    GymRat.Muscle.BICEPS: 1,
-                    GymRat.Muscle.REAR_DELTS: 1,
-                    GymRat.Muscle.TRAPS: 1,
+                    Muscle.LATS: 2,
+                    Muscle.BICEPS: 1,
+                    Muscle.REAR_DELTS: 1,
+                    Muscle.TRAPS: 1,
                 })
-            case GymRat.Routine.LEGS:
+            case Routine.LEGS:
                 self.training_queue = Counter({
-                    GymRat.Muscle.QUADS: 2,
-                    GymRat.Muscle.HAMSTRINGS: 1,
-                    GymRat.Muscle.GLUTES: 1,
-                    GymRat.Muscle.CALVES: 1,
+                    Muscle.QUADS: 2,
+                    Muscle.HAMSTRINGS: 1,
+                    Muscle.GLUTES: 1,
+                    Muscle.CALVES: 1,
                 })
-            case GymRat.Routine.TEST:
+            case Routine.TEST:
                 self.training_queue = Counter({
-                    GymRat.Muscle.CHEST: 1,
+                    Muscle.CHEST: 1,
                 })
             case _:
                 raise ValueError(f"Unsupported workout routine: {routine}")
@@ -85,23 +81,25 @@ class GymRat(mesa.Agent):
 
     def step(self):
         """advance the agent's state machine"""
-
         # NOTE: use self.model.random for random choices / time intervals, not random.random or np.rand
+        print(self)
         
         match self.state:
-            case GymRat.State.SEARCHING:
+            case State.SEARCHING:
                 free_space = [cell for cell in self.field_of_view() if self.model.equipment_layer[cell] is None]
-                print(f"free space from {self.pos}: {free_space}")
+                # print(f"free space from {self.pos}: {free_space}")
                 self.move_to(self.random.choice(free_space))
                 # TODO: follow exploration path (not random)
                 # TODO: go to a free machine, if any
             case _:
                 raise NotImplementedError(f"State {self.state} not implemented")
 
+    def __repr__(self) -> str:
+        return f"Agent {self.unique_id} @ {self.pos} ({self.state.name})"
 
     @property
     def portrayal(self) -> Dict[str, Any]:
         return {
-            "size": 10,
-            "color": "blue",
+            "size": 15,
+            "color": ["green", "red", "gray"][self.state.value - 1],
         }
--- model/gym_model.py
+++ model/gym_model.py
@@ -3,22 +3,42 @@ import mesa.space as space
 
 from enum import Enum, auto
 import numpy as np
+import copy
 from typing import List, Iterator, Optional, Dict, Any
 
 
-class Gym(mesa.Model):
-    Equipment = Enum("Equipment", "BENCH_PRESS SQUAT_RACK LEG_PRESS LAT_PULLDOWN") # TODO: add more 
-    # TODO: mapping from machine to muscle (limit to one for now)
-    # - maybe machines should be subclasses of some Machine class? (will also need mapping to visual representation)
+Equipment = Enum("Equipment", "BENCH_PRESS SQUAT_RACK LEG_PRESS LAT_PULLDOWN") # TODO: add more 
+"""machines etc. that can be used for a particular exercise"""
 
+class EquipmentAgent(mesa.Agent):
+    """piece of equipment, wrapped into an agent (for visualization purposes)"""
+
+    def __init__(self, unique_id: int, model: 'Gym', type: Equipment):
+        super().__init__(unique_id, model)
+        self.type = type
+
+    @property
+    def portrayal(self) -> Dict[str, Any]:
+        return {
+            "size": 30,
+            "color": "black",
+        }
+
+
+# TODO: mapping from machine to muscle (limit to one for now)
+# - maybe machines should be subclasses of some Machine class? (will also need mapping to visual representation)
+
+class Gym(mesa.Model):
+    num_agents: int
     agent_layer: space._Grid
     equipment_layer: np.ndarray[Optional[Equipment]]
-    
 
     def __init__(self, num_trainees: int, spawn_location: space.Coordinate = (0, 0)):
+        self.num_agents = num_trainees
+
         self.equipment_layer = np.array([ # TODO: read this from file / data structure
-            [None] * len(Gym.Equipment), # corridor
-            list(Gym.Equipment) # all the machines
+            [None] * len(Equipment), # corridor
+            list(Equipment) # all the machines
         ])
         if self.equipment_layer[spawn_location] is not None:
             raise ValueError(f"Spawn location {spawn_location} is occupied by {self.equipment_layer[spawn_location]}")
@@ -32,27 +52,38 @@ class Gym(mesa.Model):
         # - random activation by type (to avoid bias towards leg dayers??)
         
         # Create agents
-        import gym_agent # placed here to avoid circular import
+        import gym_agent as agent # placed here to avoid circular import
 
         for i in range(num_trainees):
-            a = gym_agent.GymRat(i, self)
+            a = agent.GymRat(i, self)
             self.schedule.add(a)
             self.agent_layer.place_agent(a, spawn_location)
 
         # set up data collection
         self.datacollector = mesa.datacollection.DataCollector(model_reporters={
-            "Searching": lambda m: sum(1 for a in m.agents if a.state == gym_agent.GymRat.State.SEARCHING)
+            "Searching": lambda m: sum(1 for a in m.agents if a.state == agent.State.SEARCHING)
         })
         self.running = True
 
     @property
-    def agents(self) -> List['gym_agent.GymRat']:
+    def agents(self) -> List['GymRat']:
         return self.schedule.agents
     
     @property
     def space(self) -> space._Grid:
         """space of gym elements (required by mesa visualization functions)"""
-        return self.agent_layer # TODO: merge with equipment_layer (or use custom SpaceDrawer)
+        elements = copy.copy(self.agent_layer)
+        i = self.num_agents
+
+        for cell, val in np.ndenumerate(self.equipment_layer):
+            if val is not None:
+                virtual_agent = EquipmentAgent(unique_id=i, model=self, type=val)
+                i += 1
+                elements.place_agent(virtual_agent, cell)
+
+        # could use custom space_drawer in JupyterVis instead of this hack ?
+        return elements
+    
 
     def step(self):
         self.datacollector.collect(self)
